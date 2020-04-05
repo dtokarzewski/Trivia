f9979643d8e76a7ac401fce988f8296e53fe363a
Dawid Tokarzewski <zvx@bec.dk>
2020-04-05 17:09:39 +0200

    Added styling and home screen

--- app/src/main/kotlin/dev/wellcoded/trivia/ui/HomeActivity.kt
+++ app/src/main/kotlin/dev/wellcoded/trivia/ui/HomeActivity.kt
@@ -1,6 +1,13 @@
 package dev.wellcoded.trivia.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.wellcoded.trivia.R
 
 class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 }
---
+++ feature/singleplayer/src/main/java/dev/wellcoded/singleplayer/ui/SingleplayerMenuActivity.kt
@@ -0,0 +1,6 @@
+package dev.wellcoded.singleplayer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SingleplayerMenuActivity : AppCompatActivity() {
+}
